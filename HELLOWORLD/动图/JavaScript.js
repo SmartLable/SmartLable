cbfbb1a8037f3078e33a7f15a68a46d20b501836
PJ <pjpaul@163.com>
2018-12-28 22:28:58 +0800

    动态变换

---
+++ HELLOWORLD/动图/JavaScript.js
@@ -0,0 +1,29 @@
+﻿onload = function () {
+
+    var imgs = document.getElementsByTagName('img');   // 获得标签名
+    for (var index in imgs) {
+        imgs[index].onmouseover = function () {
+            var id = this.id;
+            for (var i = 0; i < imgs.length; i++) {
+                if (i <= id) {
+                    imgs[i].src = "star2.png";
+                }
+                else {
+                    imgs[i].src = "star1.png";
+
+                }
+            }
+        }
+
+        imgs[index].onmouseout = function () {
+            var id1 = this.id;
+            for (var j = 0; j <= id1; j++) {
+                imgs[j].src = "star1.png";
+
+            }
+        }
+      
+    }
+
+
+}
